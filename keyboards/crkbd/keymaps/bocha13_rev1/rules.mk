f3683bf43a943ea769621158f38bae79fd822436
bocha13 <ignaciobochatay@gmail.com>
2023-04-06 23:27:51 -0300

    started new keymap for 3x6

---
+++ keyboards/crkbd/keymaps/bocha13_rev1/rules.mk
@@ -0,0 +1,6 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
+LTO_ENABLE      = yes
+SPLIT_KEYBOARD = yes
+UNICODE_ENABLE = yes
