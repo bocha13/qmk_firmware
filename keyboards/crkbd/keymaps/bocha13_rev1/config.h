fab8bc1bc8c5dd07d938b6a282a1f0838d35a3a8
bocha13 <ignaciobochatay@gmail.com>
2023-04-15 18:16:52 -0300

    updated configs for bocha_13_rev1, flashed to split keyboard and it works

--- keyboards/crkbd/keymaps/bocha13_rev1/config.h
+++ keyboards/crkbd/keymaps/bocha13_rev1/config.h
@@ -20,31 +20,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#undef MATRIX_ROWS
-#undef MATRIX_COLS
-#undef MATRIX_ROW_PINS
-#undef MATRIX_COL_PINS
+// #undef MATRIX_ROWS
+// #undef MATRIX_COLS
+// #undef MATRIX_ROW_PINS
+// #undef MATRIX_COL_PINS
 
-// rows are doubled-up
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 6
+// // rows are doubled-up
+// #define MATRIX_ROWS 8
+// #define MATRIX_COLS 6
 
-#define MATRIX_ROW_PINS \
-    { F6, F7, B1, B2 }
+// #define MATRIX_ROW_PINS { F6, F7, B1, B2 }
 
-// wiring of each half
-#define MATRIX_COL_PINS \
-    { D4, C6, D7, B6, B4, B5 }
+// // wiring of each half
+// #define MATRIX_COL_PINS { D4, C6, D7, B6, B4, B5 }
 
 // TRS config
 #define USE_SERIAL
-#define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT 2000
 
 /* Select hand configuration */
 #define MASTER_LEFT
 
+// #define EE_HANDS
+
+#define SPLIT_USB_DETECT
+// #define SPLIT_USB_TIMEOUT 2000
+
 // TRS data pin
-#define SOFT_SERIAL_PIN D0
+// #define SOFT_SERIAL_PIN D0
 
 #define DIODE_DIRECTION COL2ROW
--- keyboards/crkbd/keymaps/bocha13_rev1/rules.mk
+++ keyboards/crkbd/keymaps/bocha13_rev1/rules.mk
@@ -1,6 +1,2 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = no
-LTO_ENABLE      = yes
 SPLIT_KEYBOARD = yes
 UNICODE_ENABLE = yes
